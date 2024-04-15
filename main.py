9ed3c6fd3343de0a697a3b3ee4614955da960b77
DenisaRadulescu <denisa.r95@gmail.com>
2024-04-15 21:09:59 +0300

    case 3 added

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import json
+import os
+
 import image_functions as img
 import pdf_functions as pdf
 
@@ -55,8 +57,28 @@ if __name__ == '__main__':
             case "2":
                 img.save_image(image_url)
             case "3":
-                pass
-            #
+                images_list = os.listdir("images")
+                print("Available images:")
+                for i, image_name in enumerate(images_list, start=1):
+                    print(f"{i}. {image_name}")
+
+                image_choice = input("Enter the number of the image you want to view: ")
+                try:
+                    image_index = int(image_choice) - 1
+                    if 0 <= image_index < len(images_list):
+                        selected_image = os.path.join("images", images_list[image_index])
+                        try:
+                            if os.path.isfile(selected_image):
+                                img.show_images(selected_image)
+                            else:
+                                print("Image not found. Please enter a valid image number.")
+                        except Exception as e:
+                            print(f"Error displaying image: {e}")
+                    else:
+                        print("Invalid image number. Please enter a number within the range.")
+                except ValueError as e:
+                    print(f"Invalid input. Please enter a valid number {e}.")
+            #nu imi dau seama cum sa o fac
             case "4":
                 exit()
 
